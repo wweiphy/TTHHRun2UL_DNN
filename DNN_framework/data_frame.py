55990739319a40398ec1a716bf534c8c7b09fb4e
wweiphy <wei.w@cern.ch>
2022-09-27 22:33:38 -0500

    updates

--- DNN_framework/data_frame.py
+++ DNN_framework/data_frame.py
@@ -85,16 +85,19 @@ class Sample:
             # ' (((x.N_TightElectrons == 1) & (x[{}] > 0.) & (x.Electron_ReconstructionSF[0] > 0.))*x[{}]*x.Electron_ReconstructionSF[0]+((x.N_TightMuons == 1) & (x.Muon_IdentificationSF[0] > 0.) & (x.Muon_IsolationSF[0] > 0.))*x.Muon_IdentificationSF[0]*x.Muon_IsolationSF[0]) * '.format('Electron_IdentificationSF[0]', 'Electron_IdentificationSF[0]') + \
             # '(((x.N_LooseMuons == 0 & x.N_TightElectrons == 1) & (x.Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX | (x.Triggered_HLT_Ele32_WPTight_Gsf_L1DoubleEG_vX & x.Triggered_HLT_Ele32_WPTight_Gsf_2017SeedsX)))) + ((x.N_LooseElectrons == 0 & x.N_TightMuons == 1 & (x.Triggered_HLT_IsoMu27_vX)) & (x.Weight_MuonTriggerSF > 0.)) *x.Weight_MuonTriggerSF'
 
-            self.total_weight_expr = str(x['Weight_XS'] * x['Weight_CSV']  * x['Weight_GEN_nom'])  + \
-            str( * x['Weight_pu69p2']  * x['Weight_L1ECALPrefire'])  + \
-            str( * (((x['N_TightElectrons'] == 1) & (x['Electron_IdentificationSF[0]'] > 0.) & (x['Electron_ReconstructionSF[0]'] > 0.))*x['Electron_IdentificationSF[0]']*x['Electron_ReconstructionSF[0]'] + ((x['N_TightMuons'] == 1) & (x['Muon_IdentificationSF[0]'] > 0.) & (x['Muon_IsolationSF[0]'] > 0.))*x['Muon_IdentificationSF[0]'] * x['Muon_IsolationSF[0]']))  + \
-            str( * (((x['N_LooseMuons']  == 0 & x['N_TightElectrons']  == 1) & (x['Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX']  | (x['Triggered_HLT_Ele32_WPTight_Gsf_L1DoubleEG_vX']  & x['Triggered_HLT_Ele32_WPTight_Gsf_2017SeedsX'])))) + ((x['N_LooseElectrons']  == 0 & x['N_TightMuons']  == 1 & (x['Triggered_HLT_IsoMu27_vX'])) & (x['Weight_MuonTriggerSF'] > 0.)) * x['Weight_MuonTriggerSF'] )
+            self.total_weight_expr = 'x['+'Weight_XS'+'] * x['+'Weight_CSV'+']  * x['+'Weight_GEN_nom'+']'  + \
+             '* x['+'Weight_pu69p2'+']  * x['+'Weight_L1ECALPrefire'+']'  + \
+             '* (((x['+'N_TightElectrons'+'] == 1) & (x['+'Electron_IdentificationSF[0]'+'] > 0.) & (x['+'Electron_ReconstructionSF[0]'+'] > 0.))*x['+'Electron_IdentificationSF[0]'+']*x['+'Electron_ReconstructionSF[0]'+'] + ((x['+'N_TightMuons'+'] == 1) & (x['+'Muon_IdentificationSF[0]'+'] > 0.) & (x['+'Muon_IsolationSF[0]'+'] > 0.))*x['+'Muon_IdentificationSF[0]'+'] * x['+'Muon_IsolationSF[0]'+'])'  + \
+             '* (((x['+'N_LooseMuons'+']  == 0 & x['+'N_TightElectrons'+']  == 1) & (x['+'Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX'+']  | (x['+'Triggered_HLT_Ele32_WPTight_Gsf_L1DoubleEG_vX'+']  & x['+'Triggered_HLT_Ele32_WPTight_Gsf_2017SeedsX'+'])))) + ((x['+'N_LooseElectrons'+']  == 0 & x['+'N_TightMuons'+']  == 1 & (x['+'Triggered_HLT_IsoMu27_vX'+'])) & (x['+'Weight_MuonTriggerSF'+'] > 0.)) * x['+'Weight_MuonTriggerSF'+'] '
+
+            # self.total_weight_expr = self.total_weight_expr + '* x['Electron_IdentificationSF[0]']'
         
 
             print (self.total_weight_expr)
 
         # add event weight
-        df = df.assign(total_weight=lambda x: eval(self.total_weight_expr))
+        # df = df.assign(total_weight=lambda x: eval(self.total_weight_expr))
+        df = df.assign(total_weight=lambda x: (self.total_weight_expr))
         print("total weight: {}".format(df["total_weight"].values))
         # assign train weight
         weight_sum = sum(df["total_weight"].values)
