bdb97b2995a6289ca40f6bd3d361576f42800ce0
wweiphy <wei.w@cern.ch>
2022-09-30 11:08:38 -0500

    test

--- DNN_framework/data_frame.py
+++ DNN_framework/data_frame.py
@@ -47,14 +47,14 @@ class Sample:
         self.nevents = df.shape[0]
         
         if Do_Evaluation:
-            # LeptonSF = SFs.LeptonSF()
+            LeptonSF = SFs.LeptonSF()
             # print("example of the df element: ")
             # print(df["Electron_IdentificationSF[0]"][0])
-            # ElectronTriggerSF = []
-            # for i in range(df.shape[0]):
-            #     ElectronTriggerSF.append(LeptonSF.GetElectronSF(df['Electron_Pt[0]'][i], df['Electron_Eta[0]'][i], syst = '', type = "Trigger"))
-            # self.ElectronTriggerSF = np.array(ElectronTriggerSF)
-            # print(ElectronTriggerSF)
+            ElectronTriggerSF = []
+            for i in range(df.shape[0]):
+                ElectronTriggerSF.append(LeptonSF.GetElectronSF(df['Electron_Pt[0]'][i], df['Electron_Eta[0]'][i], syst = '', type = "Trigger"))
+            self.ElectronTriggerSF = np.array(ElectronTriggerSF)
+            print(ElectronTriggerSF)
             # df['internalEleTriggerWeight'] = self.ElectronTriggerSF.tolist()
 
             # BTagSFs = SFs.BTagSF()
@@ -76,12 +76,6 @@ class Sample:
             # self.BTagSF = np.array(BTagSF)
 
 
-    
-            # add event weight
-            # df = df.assign(total_weight=lambda x: eval(self.total_weight_expr))
-            # df = df.assign(total_weight=lambda x: self.total_weight_expr)
-
-
             df = df.assign(sf_weight=lambda x: (x['Weight_pu69p2'] * x['Weight_L1ECALPrefire'] * (((x['N_TightElectrons'] == 1) & (x['Electron_IdentificationSF[0]'] > 0.) & (x['Electron_ReconstructionSF[0]'] > 0.))*1.*x['Electron_IdentificationSF[0]']*x['Electron_ReconstructionSF[0]'] + ((x['N_TightMuons'] == 1) & (x['Muon_IdentificationSF[0]'] > 0.) & (x['Muon_IsolationSF[0]'] > 0.))*1.*x['Muon_IdentificationSF[0]'] * x['Muon_IsolationSF[0]']) * ((((x['N_LooseMuons'] == 0) & (x['N_TightElectrons'] == 1)) & ((x['Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX'] == 1) | (
             (x['Triggered_HLT_Ele32_WPTight_Gsf_L1DoubleEG_vX'] == 1) & (x['Triggered_HLT_Ele32_WPTight_Gsf_2017SeedsX'] == 1)))) * 1. + (((x['N_LooseElectrons'] == 0) & (x['N_TightMuons'] == 1) & (x['Triggered_HLT_IsoMu27_vX'])) & (x['Weight_MuonTriggerSF'] > 0.)) * 1. * x['Weight_MuonTriggerSF'])))
 
@@ -94,9 +88,7 @@ class Sample:
                 print("2")
                 df = df.assign(extra_weight=lambda x: 1.)
 
-            # df = df.assign(xs_weight=lambda x: eval(self.total_weight_expr))
-            df = df.assign(xs_weight=lambda x: x.Weight_XS *
-                           x.Weight_CSV * x.Weight_GEN_nom)
+            df = df.assign(xs_weight=lambda x: eval(self.total_weight_expr))
             df = df.assign(total_weight=lambda x: x.xs_weight * x.extra_weight * x.sf_weight)
 
         # print("total weight: ")
