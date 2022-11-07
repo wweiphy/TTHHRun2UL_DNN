c9ba1bf2be6b5f1ead90de90bd13facf994b982e
wweiphy <wei.w@cern.ch>
2022-11-07 09:39:24 -0600

    updates

--- datacard/cardmakingscirpt.py
+++ datacard/cardmakingscirpt.py
@@ -8,8 +8,10 @@ parser.add_option("-n", "--new", dest="new", default=True,
 
 (options, args) = parser.parse_args()
 
-process_new = [ttHH, ttZH, ttZZ, ttZbb, ttH, ttlf, ttcc, ttmb, ttnb]
-process_old = [ttHH, ttZH, ttZZ, ttZbb, ttH, ttlf, ttcc, ttb, ttbb, tt2b, ttbbb, tt4b]
+process_new = ['ttHH', 'ttH', 'ttZ', 'ttZH',
+               'ttZZ', 'ttlf', 'ttcc', 'ttmb', 'ttnb']
+process_old = ['ttHH', 'ttH', 'ttZ', 'ttZH',
+               'ttZZ', 'ttlf', 'ttcc', 'ttb', 'ttbb', 'tt2b', 'ttbbb', 'tt4b']
 
 
 if options.new:
@@ -17,7 +19,7 @@ if options.new:
     for node in process_new:
 
         categoryname = "ljets_ge4j_ge3t_{}_node".format(node)
-        rootfile = ""
+        rootfile = "/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/221107_test_evaluation_new/plots/output_limit.root"
         outputfile = "/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/new/ljets_ge4j_ge3t_{}_node_hdecay.txt".format(node)
         signaltag = node
 
