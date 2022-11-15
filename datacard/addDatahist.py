179efe325a0abfe0a0863a0125845b200378f729
wweiphy <wei.w@cern.ch>
2022-11-15 09:07:45 -0600

    updates

--- datacard/addDatahist.py
+++ datacard/addDatahist.py
@@ -19,7 +19,7 @@ classNames = ['ttHH','ttH','ttZ','ttZH','ttZZ','ttlf','ttcc','ttb','tt2b','ttbb'
 
 # hadd -f output_limit.root 1 2 3
 
-filepath = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/221113_test_evaluation_old/plots/output_limit.root'
+filepath = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/221114_test_evaluation_old/plots/output_limit.root'
 
 
 print(classNames)
