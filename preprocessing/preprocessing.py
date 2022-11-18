c18e0cc92c82b1fbab44194172d7d7f04d9099de
wweiphy <wei.w@cern.ch>
2022-11-17 22:20:58 -0600

    test

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -572,6 +572,8 @@ class Dataset:
 
             for j in range(jet_pt["Jet_Pt"][i].size):
                 print(abs(jet_eta['Jet_Eta'][i][j]))
+                sf = btvjson["deepJet_shape"].evaluate("central", jet_flavor['Jet_Flav'][i][j], abs(
+                    jet_eta['Jet_Eta'][i][j]), jet_pt['Jet_Pt'][i][j], jet_bTag['Jet_CSV'][i][j])
                 jet_sf_perevent *= btvjson["deepJet_shape"].evaluate("central", jet_flavor['Jet_Flav'][i][j], abs(jet_eta['Jet_Eta'][i][j]), jet_pt['Jet_Pt'][i][j], jet_bTag['Jet_CSV'][i][j])
             jet_sf.append(jet_sf_perevent)
 
