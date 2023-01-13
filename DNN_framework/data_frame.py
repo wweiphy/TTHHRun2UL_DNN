c6fc13e683ab4cbb6de3fcdf342571156130a05a
wweiphy <wei.w@cern.ch>
2023-01-13 10:06:43 -0600

    updates

--- DNN_framework/data_frame.py
+++ DNN_framework/data_frame.py
@@ -154,6 +154,17 @@ class Sample:
                 df = df.assign(total_preweight=lambda x: (x['lumiWeight']* x['Weight_XS'] * x['Weight_CSV_UL'] * x['Weight_GEN_nom'] * x['Weight_pu69p2'] * x['Weight_JetPUID'] * x['Weight_L1ECALPrefire'] * (((x['N_TightElectrons'] == 1) & (x['Electron_IdentificationSFUp[0]'] > 0.) & (x['Electron_ReconstructionSFUp[0]'] > 0.))*1.*x['Electron_IdentificationSFUp[0]']*x['Electron_ReconstructionSFUp[0]'] + ((x['N_TightMuons'] == 1) & (x['Muon_IdentificationSF[0]'] > 0.) & (x['Muon_ReconstructionSF[0]'] > 0.) & (x['Muon_IsolationSF[0]'] > 0.))*1.*x['Muon_IdentificationSF[0]'] * x['Muon_IsolationSF[0]'] * x['Muon_ReconstructionSF[0]']) * ((((x['N_LooseMuons'] == 0) & (x['N_TightElectrons'] == 1)) & ((x['Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX'] == 1) | (
                     (x['Triggered_HLT_Ele32_WPTight_Gsf_L1DoubleEG_vX'] == 1) & (x['Triggered_HLT_Ele32_WPTight_Gsf_2017SeedsX'] == 1))) & (x['Weight_ElectronTriggerSF'] > 0)) * 1. * x['Weight_ElectronTriggerSF'] + (((x['N_LooseElectrons'] == 0) & (x['N_TightMuons'] == 1) & (x['Triggered_HLT_IsoMu27_vX'])) & (x['Weight_MuonTriggerSF'] > 0.)) * 1. * x['Weight_MuonTriggerSF'])))
                 
+                df.loc[:, 'Weight_scale_variation_muR_0p5_muF_0p5'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_0p5_muF_1p0'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_0p5_muF_2p0'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_1p0_muF_0p5'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_1p0_muF_1p0'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_1p0_muF_2p0'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_2p0_muF_0p5'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_2p0_muF_1p0'] = 1.
+                df.loc[:, 'Weight_scale_variation_muR_2p0_muF_2p0'] = 1.
+
+
                 if self.label == "ttlf" or self.label == "ttcc" or self.label == "ttb" or self.label == "ttbb" or self.label == "tt2b" or self.label == "ttmb":
                     df['total_preweight'] = df['total_preweight'] * \
                         df['Weight_scale_variation_muR_0p5_muF_0p5'].abs().le(
@@ -181,27 +192,6 @@ class Sample:
                     df['total_weight_PDF_Weight_{}'.format(x)] = (df['Weight_pdf_variation_{}'.format(
                         x)]*((df['process'] == "ttbbSL")*1. * float(internal_ttbb['ttbbSL'][internal_ttbb[internal_ttbb['Name'] == 'Weight_pdf_variation_{}'.format(x)].index]) + (df['process'] == "ttbbDL")*1. * float(internal_ttbb['ttbbDL'][internal_ttbb[internal_ttbb['Name'] == 'Weight_pdf_variation_{}'.format(x)].index]))) * df['total_preweight']*df['compare']*1.
                 
-                # print(float(internal['ttSL'][internal[internal['Name']
-                #       == 'Weight_scale_variation_muR_2p0_muF_1p0'].index]))
-                # print(float(internal['ttDL'][internal[internal['Name']
-                #       == 'Weight_scale_variation_muR_2p0_muF_1p0'].index]))
-                # print(float(internal_ttbb['ttbbSL'][internal_ttbb[internal_ttbb['Name']
-                #       == 'Weight_scale_variation_muR_2p0_muF_1p0'].index]))
-                # print(float(internal_ttbb['ttbbSL'][internal_ttbb[internal_ttbb['Name']
-                #       == 'Weight_scale_variation_muR_2p0_muF_1p0'].index]))
-                
-                # print(df['Weight_scale_variation_muR_2p0_muF_1p0'][0])
-
-                df.loc[:, 'Weight_scale_variation_muR_0p5_muF_0p5'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_0p5_muF_1p0'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_0p5_muF_2p0'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_1p0_muF_0p5'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_1p0_muF_1p0'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_1p0_muF_2p0'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_2p0_muF_0p5'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_2p0_muF_1p0'] = 0.
-                df.loc[:, 'Weight_scale_variation_muR_2p0_muF_2p0'] = 0.
-
 
                 # if (self.label != "ttb" or self.label != "ttbb" or self.label != "tt2b" or self.label != "ttmb" or self.label != "ttH" or self.label != "ttcc" or self.label != "ttlf"):
                     
