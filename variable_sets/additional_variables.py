dc97c4b5db6852df67c01f769b01e0ef9dbf2f05
wweiphy <wei.w@cern.ch>
2023-01-17 15:17:09 -0600

    updates

--- variable_sets/additional_variables.py
+++ variable_sets/additional_variables.py
@@ -5,12 +5,16 @@ additional_variables = [
     "N_BTagsL",
     "N_BTagsM",
     "N_BTagsT",
+    'N_TightElectrons',
+    'N_TightMuons',
     "Weight_XS",
     "Weight_CSV",
     "Weight_GEN_nom",
     "Evt_ID",
     "Evt_Run",
-    "Evt_Lumi"
+    "Evt_Lumi",
+    'N_LooseElectrons',
+    'N_LooseMuons'
     ]
 
 # all_additional_variables = list(set([v for v in additional_variables]))
