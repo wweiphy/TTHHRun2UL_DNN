326653db6610ae7aeb6c4dda578adff8eaeeff48
wweiphy <wei.w@cern.ch>
2023-01-17 21:48:26 -0600

    fix

--- datacard/mergeSystScripts/mergeSysts_ttHH.sh
+++ datacard/mergeSystScripts/mergeSysts_ttHH.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230103_evaluation_old/plots/ttHH_discriminator.root'
+export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230103_evaluation_new/plots/ttHH_discriminator.root'
 export SEPARATOR='__'
 export NOMHISTKEY='$CHANNEL__$PROCESS'
 export ORIGNAME='ttHH'
