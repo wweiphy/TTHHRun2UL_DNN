46d2b9359f1d857a40499f6d9b13caf74bc8e523
wweiphy <wei.w@cern.ch>
2023-01-17 21:47:03 -0600

    Include systematics merging script

---
+++ datacard/mergeSystScripts/mergeSysts_ttHH.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230103_evaluation_old/plots/ttHH_discriminator.root'
+export SEPARATOR='__'
+export NOMHISTKEY='$CHANNEL__$PROCESS'
+export ORIGNAME='ttHH'
+export SYSTHISTKEY='$CHANNEL__$PROCESS__$SYSTEMATIC'
+
+python /uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/mergeSyst.py
