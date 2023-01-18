f3b9881623062a61cc923ce28e877b672ea6c914
wweiphy <wei.w@cern.ch>
2023-01-17 21:50:21 -0600

    update PDF uncertainties

--- datacard/pdf_relic_names.py
+++ datacard/pdf_relic_names.py
@@ -7,40 +7,40 @@ names_alphaS = ["Weight_pdf_variation_{}".format(x)
 names_mcrelic = ["Weight_pdf_variation_{}".format(x)
             for x in range(320900, 321001)]
 config = {
-    "CMS_ttHbb_PDF" : {
+    "PDF" : {
         "to_replace" : "PDF_RELIC",
         "construction" : "Hessian",
         "expand_with" : names,
         "merge_with" : ["CMS_ttHbb_PDF_alphaS"]
     },
-    "CMS_ttHbb_PDF_alphaS" : {
+    "PDF_alphaS" : {
         "to_replace" : "PDF_RELIC",
         "construction" : "Hessian",
         "expand_with" : names_alphaS
     },
-    "CMS_ttHbb_PDF_tHq" : {
-        "to_replace" : "PDF_RELIC",
-        "construction" : "Hessian",
-        "expand_with" : names,
-        "merge_with" : ["CMS_ttHbb_PDF_alphaS_tHq"]
-    },
-    "CMS_ttHbb_PDF_alphaS_tHq" : {
-        "to_replace" : "PDF_RELIC",
-        "construction" : "Hessian",
-        "expand_with" : names_alphaS
-    },
-    "CMS_ttHbb_PDF_tHW" : {
-        "to_replace" : "PDF_RELIC",
-        "construction" : "Hessian",
-        "expand_with" : names,
-        "merge_with" : ["CMS_ttHbb_PDF_alphaS_tHW"]
-    },
-    "CMS_ttHbb_PDF_alphaS_tHW" : {
-        "to_replace" : "PDF_RELIC",
-        "construction" : "Hessian",
-        "expand_with" : names_alphaS
-    },
-    "CMS_ttHbb_PDF_ttbbNLO" : {
+    # "CMS_ttHbb_PDF_tHq" : {
+    #     "to_replace" : "PDF_RELIC",
+    #     "construction" : "Hessian",
+    #     "expand_with" : names,
+    #     "merge_with" : ["CMS_ttHbb_PDF_alphaS_tHq"]
+    # },
+    # "CMS_ttHbb_PDF_alphaS_tHq" : {
+    #     "to_replace" : "PDF_RELIC",
+    #     "construction" : "Hessian",
+    #     "expand_with" : names_alphaS
+    # },
+    # "CMS_ttHbb_PDF_tHW" : {
+    #     "to_replace" : "PDF_RELIC",
+    #     "construction" : "Hessian",
+    #     "expand_with" : names,
+    #     "merge_with" : ["CMS_ttHbb_PDF_alphaS_tHW"]
+    # },
+    # "CMS_ttHbb_PDF_alphaS_tHW" : {
+    #     "to_replace" : "PDF_RELIC",
+    #     "construction" : "Hessian",
+    #     "expand_with" : names_alphaS
+    # },
+    "PDF_ttbbNLO" : {
         "to_replace" : "PDF_RELIC",
         "construction" : "MCrelic",
         "expand_with" : names_mcrelic,
