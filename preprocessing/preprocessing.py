be071b0f925ca6ff13077ad23301d783be0ed281
wweiphy <wei.w@cern.ch>
2023-01-25 15:59:54 -0600

    updates

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -485,7 +485,7 @@ class Dataset:
                                 df['compare'] = df['Weight_pdf_variation_{}'.format(x)].ge(
                                     0.)
                                 df['total_weight_PDF_Weight_{}'.format(x)] = (df['Weight_pdf_variation_{}'.format(
-                                    x)]*((df['process'] == "ttSL")*1. * float(internal['ttSL'][internal[internal['Name'] == 'Weight_pdf_variation_{}'.format(x)].index]) + (df['process'] == "ttDL")*1. * float(internal['ttDL'][internal[internal['Name'] == 'Weight_pdf_variation_{}'.format(x)].index]))) * df['total_preweight']*df['compare']*1.
+                                    x)]*((df['process'] == "ttSL")*1. * float(internal['ttSL'][internal[internal['Name'] == 'Weight_pdf_variation_{}'.format(x)].index]) + (df['process'] == "ttDL")*1. * float(internal['ttDL'][internal[internal['Name'] == 'Weight_pdf_variation_{}'.format(x)].index]) + (df['process'] == "ttH")*1. * float(internal['ttH'][internal[internal['Name'] == 'Weight_pdf_variation_{}'.format(x)].index]))) * df['total_preweight']*df['compare']*1.
                             else:
 
                                 df.loc[:, 'Weight_pdf_variation_{}'.format(
