1a5614a432be72dbf06780620dbf4304bc4f8b8d
wweiphy <wei.w@cern.ch>
2023-01-24 22:03:23 -0600

    Updates

--- datacard/mergeSystScripts/mergeSysts_ttmb.sh
+++ datacard/mergeSystScripts/mergeSysts_ttmb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230103_evaluation_new/plots/output_limit.root'
+export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230119_evaluation_new/plots/output_limit.root'
 export SEPARATOR='__'
 export NOMHISTKEY='$CHANNEL__$PROCESS'
 export ORIGNAME='ttmb'
