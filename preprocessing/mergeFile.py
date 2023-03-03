b7edaccf5a0fdf8bba4d2348c77dabea2b38ab52
wweiphy <wei.w@cern.ch>
2023-03-03 10:26:05 -0600

    updates

---
+++ preprocessing/mergeFile.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+with pd.HDFStore("/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_12_1_1/src/TTHHRun2UL_DNN/workdir/ttlf_dnn.h5", "a") as store:
+    store.append("data", pd.read_hdf("/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_12_1_1/src/TTHHRun2UL_DNN/workdir/Eval_0220_UL_nominal_3/ttlf_dnn.h5"),index=False)
+    store.append("data", pd.read_hdf("/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_12_1_1/src/TTHHRun2UL_DNN/workdir/Eval_0220_UL_nominal_4/ttlf_dnn.h5"),index=False)
+
+print ("finished merging ttlf files")
+
+with pd.HDFStore("/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_12_1_1/src/TTHHRun2UL_DNN/workdir/ttcc_dnn.h5", "a") as store:
+    store.append("data", pd.read_hdf("/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_12_1_1/src/TTHHRun2UL_DNN/workdir/Eval_0220_UL_nominal_3/ttcc_dnn.h5"),index=False)
+    store.append("data", pd.read_hdf("/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_12_1_1/src/TTHHRun2UL_DNN/workdir/Eval_0220_UL_nominal_4/ttcc_dnn.h5"),index=False)
+
+
+print ("finished merging ttcc files")
