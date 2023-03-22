8cc85a8ae39312a10f507d73e2d624562b6f5735
wweiphy <wei.w@cern.ch>
2023-03-21 22:31:31 -0500

    update

--- datacard/mergeSystScripts/mergeSysts_ttH.sh
+++ datacard/mergeSystScripts/mergeSysts_ttH.sh
@@ -5,9 +5,9 @@
 # cd /uscms_data/d3/wwei/SM_TTHH/CMSSW_11_1_0_pre4/src
 # eval `scram runtime -sh`
 # cd - 
-export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new/plots/output_limit.root'
+# export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new/plots/output_limit.root'
 # export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_old/plots/output_limit.root'
-# export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new_5j4b/plots/output_limit.root'
+export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new_5j4b/plots/output_limit.root'
 # export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new_6j5b/plots/output_limit.root'
 export SEPARATOR='__'
 export NOMHISTKEY='$CHANNEL__$PROCESS'
