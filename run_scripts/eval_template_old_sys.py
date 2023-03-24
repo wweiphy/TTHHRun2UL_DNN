42302a8004861deabbeba336b463758b2ae53b5f
wweiphy <wei.w@cern.ch>
2023-03-24 09:51:17 -0500

    updates

--- run_scripts/eval_template_old_sys.py
+++ run_scripts/eval_template_old_sys.py
@@ -15,8 +15,10 @@ import DNN_framework.DNN as DNN
 import DNN_framework.data_frame as df
 
 
-# python eval_template_old_sys.py -o 230220_evaluation_old -i 230220_50_old_ge4j_ge3t  --signalclass=ttHH --plot --printroc -d Eval_0305_UL --syst=JERup
+# python eval_template_old_sys.py -o 230220_evaluation_old -i 230220_50_old_ge4j_ge3t  --signalclass=ttHH --plot --printroc -d Eval_0308_UL --syst=JESup
 
+# 2017
+# python eval_template_old_sys.py -o 230119_evaluation_old_2 -i 221130_50_old_ge4j_ge3t --signalclass=ttHH --plot --printroc -d Eval_0119_UL --syst=JESup
 
 
 """
@@ -171,15 +173,15 @@ for sample in config["eventClasses"]:
                 # sample_path = dfDirectory+"ttZ_dnn.h5"
         elif sample["sampleLabel"] == "ttb":
                 # sample_train_weight = 1
-                normalization_weight = 6.10
+                normalization_weight = 61.
                 # sample_path = dfDirectory+"ttb_dnn.h5"
         elif sample["sampleLabel"] == "ttbb":
                 # sample_train_weight = 1
-                normalization_weight = 6.10
+                normalization_weight = 61.
                 # sample_path = dfDirectory+"ttbb_dnn.h5"
         elif sample["sampleLabel"] == "tt2b":
         #     sample_train_weight = 1
-                normalization_weight = 6.10 
+                normalization_weight = 61.
         #     sample_path = dfDirectory+"tt2b_dnn.h5"
         elif sample["sampleLabel"] == "tt4b":
         #     sample_train_weight = 1
