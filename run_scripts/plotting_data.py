ae6ea384f23c0cec652be766f3d35bbb12950b50
wweiphy <wei.w@cern.ch>
2023-03-24 23:13:10 -0500

    updates

---
+++ run_scripts/plotting_data.py
@@ -0,0 +1,85 @@
+
+# 2017
+
+# python plotting_ttZZ.py -i Eval_0119_UL_nominal -o ttZZ -c ge4j_ge3t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables -n ge4j_ge3t_ttH --plot --printroc
+
+
+# global imports
+# import ROOT
+# ROOT.PyConfig.IgnoreCommandLineOptions = True
+import os
+import sys
+import optparse
+import json
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(filedir)
+sys.path.append(basedir)
+
+import optionHandler
+options = optionHandler.optionHandler(sys.argv)
+
+# local imports
+filedir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(filedir)
+sys.path.append(basedir)
+
+# import class for DNN training
+import DNN_framework.DNN as DNN
+import DNN_framework.data_frame as df
+
+options.initArguments()
+
+
+input_samples = df.InputSamples(options.getInputDirectory(), options.getTestPercentage())
+
+weight_expr = "x.Weight_XS * x.Weight_CSV_UL * x.Weight_GEN_nom * x.lumiWeight"
+
+input_samples.addSample(options.getDefaultName("ttHH"),  label = "ttHH",  normalization_weight = options.getNomWeight(), train_weight = 1, total_weight_expr = weight_expr)
+
+input_samples.addSample(options.getDefaultName("ttmb"),  label = "ttmb",  normalization_weight = 61., total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttcc"), label = "ttcc", normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttlf"), label = "ttlf", normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttnb"), label = "ttnb", normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttH"), label = "ttH", normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttZH"), label = "ttZH", normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttZZ"), label = "ttZZ", normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttZ"), label = "ttZ", normalization_weight = options.getNomWeight(), total_weight_expr = weight_expr)
+
+
+# init DNN class
+# dnn = DNN.DNN(
+# save_path=outPath,
+# # sample_save_path=sample_save_path,
+# input_samples=input_samples,
+# lumi = 119.4,
+# # lumi = 41.5,
+# category_name=config["JetTagCategory"],
+# train_variables=config["trainVariables"],
+# Do_Evaluation = True,
+# shuffle_seed=config["shuffleSeed"],
+# addSampleSuffix=config["addSampleSuffix"],
+# )
+
+dnn = DNN.DNN(
+    save_path       = options.getOutputDir(),
+    input_samples   = input_samples,
+    category_name   = options.getCategory(),
+    train_variables = options.getTrainVariables(),
+    # number of epochs
+    lumi = 41.5,
+    # lumi = 119.4,
+    train_epochs    = options.getTrainEpochs(),
+    # metrics for evaluation (c.f. KERAS metrics)
+    eval_metrics    = ["acc"],
+    Do_Evaluation   = True,
+    # percentage of train set to be used for testing (i.e. evaluating/plotting after training)
+    test_percentage = options.getTestPercentage(),
+    # balance samples per epoch such that there amount of samples per category is roughly equal
+    # balanceSamples  = options.doBalanceSamples(),
+    evenSel         = options.doEvenSelection()
+    )
+
+
+
+dnn.save_DNNInput(node_cls="ttZZ", isData=True) 
