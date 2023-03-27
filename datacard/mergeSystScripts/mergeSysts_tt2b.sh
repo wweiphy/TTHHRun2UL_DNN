4312e25cd9f9c77be4c885f20922bbabe599f8ac
wweiphy <wei.w@cern.ch>
2023-03-26 22:18:35 -0500

    updates

--- datacard/mergeSystScripts/mergeSysts_tt2b.sh
+++ datacard/mergeSystScripts/mergeSysts_tt2b.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_old/plots/output_limit.root'
+# export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_old/plots/output_limit.root'
+export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230119_evaluation_old_2/plots/output_limit.root'
 export SEPARATOR='__'
 export NOMHISTKEY='$CHANNEL__$PROCESS'
 export ORIGNAME='tt2b'
