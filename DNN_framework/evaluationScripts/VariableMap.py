9d878e92cd9767cc9b1b53544589171a3bd3f860
wweiphy <wei.w@cern.ch>
2023-04-11 16:48:56 -0500

    updates

--- DNN_framework/evaluationScripts/VariableMap.py
+++ DNN_framework/evaluationScripts/VariableMap.py
@@ -2,7 +2,7 @@ MCVariable = {
 
 'Evt_CSV_avg': [50,0,1],
 'Evt_CSV_avg_tagged':[50,0,1],
-'Evt_CSV_dev':[50,-1,1],
+'Evt_CSV_dev':[50,0,0.4],
 
 
 }
@@ -11,7 +11,7 @@ DataVariable = {
 
 'Evt_CSV_avg': [50,0,1],
 'Evt_CSV_avg_tagged':[50,0,1],
-'Evt_CSV_dev':[50,-1,1],
+'Evt_CSV_dev':[50,0,0.4],
 
 }
 
