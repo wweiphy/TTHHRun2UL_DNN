7c992591b6db62f5d20846fd6e56a7cdd762ec48
wweiphy <wei.w@cern.ch>
2023-06-23 14:50:04 -0500

    update

--- datacard/plotconfig_new.py
+++ datacard/plotconfig_new.py
@@ -94,8 +94,8 @@ plotoptions = {   'cmslabel': 'private Work',
     'datalabel': 'data',
     'logarithmic': False,
     # 'lumiLabel': '59.8',
-    'lumiLabel': '41.5',
-    # 'lumiLabel': '16.81', # post
+    # 'lumiLabel': '41.5',
+    'lumiLabel': '16.81', # post
     # 'lumiLabel': '19.52', # pre
     'nominalKey': '$CHANNEL__$PROCESS',
     'normalize': False,
