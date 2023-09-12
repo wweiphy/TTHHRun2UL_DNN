82b4f463f7001f09e7d29bb6c207238635eaaf47
wweiphy <wei.w@cern.ch>
2023-09-11 20:48:21 -0500

    update

--- run_scripts/plotting_ttZH.py
+++ run_scripts/plotting_ttZH.py
@@ -9,7 +9,10 @@
 # python plotting_ttZH.py -i Eval_0515_UL_nominal -o ttZH_2016pre -c ge4j_ge3t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables -n ge4j_ge3t_ttH --plot --printroc
 
 # python plotting_ttZH.py -i Control_0409 -o ttZH_3 -c ge4j_2t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables --plot --printroc
+
 # python plotting_ttZH.py -i Control_0822_2017 -o ttZH_2017 -c ge4j_2t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables --plot --printroc
+
+# python plotting_ttZH.py -i Control_1718 -o ttZH_1718 -c ge4j_2t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables --plot --printroc
 
 
 # global imports
@@ -42,7 +45,8 @@ options.initArguments()
 input_samples = df.InputSamples(options.getInputDirectory(), options.getTestPercentage())
 
 weight_expr = "x.Weight_XS * x.Weight_CSV_UL * x.Weight_GEN_nom * x.lumiWeight"
-input_samples.addSample(options.getDefaultName("ttZH"),  label = "ttZH",  normalization_weight = options.getNomWeight(), train_weight = 1, total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttZH_2017"),  label = "ttZH",  normalization_weight = 83., train_weight = 1, total_weight_expr = weight_expr)
+input_samples.addSample(options.getDefaultName("ttZH_2018"),  label = "ttZH",  normalization_weight = 119.4, train_weight = 1, total_weight_expr = weight_expr)
 
 # init DNN class
 # dnn = DNN.DNN(
@@ -67,7 +71,8 @@ dnn = DNN.DNN(
     # lumi = 119.4,
     # lumi=67.24,  # 2016post
     # lumi = 78.08, # 2016pre
-    lumi = 83,
+    # lumi = 83,
+    lumi = 1.,
     train_epochs    = options.getTrainEpochs(),
     # metrics for evaluation (c.f. KERAS metrics)
     eval_metrics    = ["acc"],
