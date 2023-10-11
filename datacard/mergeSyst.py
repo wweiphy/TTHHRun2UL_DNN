07660d22cb58168252a6fd7e1f01c30e9196d47b
wweiphy <wei.w@cern.ch>
2023-10-10 21:45:50 -0500

    update script for 5FS tt samples

--- datacard/mergeSyst.py
+++ datacard/mergeSyst.py
@@ -8,12 +8,13 @@ process      = os.getenv('ORIGNAME')
 nom_key      = os.getenv('NOMHISTKEY')
 syst_key     = os.getenv('SYSTHISTKEY')
 separator    = os.getenv('SEPARATOR')
-syst_csvpath = "/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/systematics_full.csv"
+syst_csvpath = "/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/systematics_full_5FS.csv"
+# syst_csvpath = "/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/systematics_full.csv"
 print(process)
 combine_intermid_systs.combine_intermid_syst(   h_nominal_key   = nom_key, 
                                                 h_syst_key      = syst_key, 
                                                 rfile_path      = filename,
-                                                replace_config  = "/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/pdf_relic_names",
+                                                replace_config  = "/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/pdf_relic_names_5FS",
                                                 processes       = process,
                                                 separator       = separator,
                                                 syst_csvpath    = syst_csvpath
--- datacard/mergeSystScripts/runningscript_new_5FS.sh
+++ datacard/mergeSystScripts/runningscript_new_5FS.sh
@@ -10,8 +10,12 @@
 # export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230515_evaluation_new_6j4b_2/plots/output_limit.root'
 # export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230515_evaluation_new_5j4b_2/plots/output_limit.root'
 
+# 2018 5FS
+# export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new_5FS/plots/output_limit.root'
+# export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new_5j4b_5FS/plots/output_limit.root'
+# export INFILE='/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/workdir/230220_evaluation_new_6j4b_5FS/plots/output_limit.root'
+
 /uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/mergeSystScripts/mergeSysts_ttH.sh
 /uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/mergeSystScripts/mergeSysts_ttcc.sh
 /uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/mergeSystScripts/mergeSysts_ttlf.sh
-# Todo - comment out if applying on 5FS
-# /uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/mergeSystScripts/mergeSysts_ttmb.sh
+/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_11_1_2/src/TTHHRun2UL_DNN/datacard/mergeSystScripts/mergeSysts_ttmb.sh
