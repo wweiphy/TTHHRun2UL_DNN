e1560935b5ad72d42985030323f2e773879bc019
wweiphy <wei.w@cern.ch>
2023-10-17 22:13:03 -0500

    update

--- run_scripts/eval_template_new_sys.py
+++ run_scripts/eval_template_new_sys.py
@@ -210,7 +210,8 @@ for sample in config["eventClasses"]:
                 #     sample_train_weight = 1
                 # normalization_weight = 1.
                 # normalization_weight = 1.35 # for 2018 tt4b
-                normalization_weight = 3.538023785  # for 2018 ttbb 5j4b
+                # normalization_weight = 3.538023785  # for 2018 ttbb 5j4b
+                normalization_weight = 3.538023785 * 2  # for 2018 ttbb 5j4b JER down
                 # normalization_weight = 3.363282228  # for 2018 ttbb 6j4b
                 # normalization_weight = 1.240415029  # for 2018 tt4b 5j4b
                 # normalization_weight = 1.212174627  # for 2018 tt4b 6j4b
