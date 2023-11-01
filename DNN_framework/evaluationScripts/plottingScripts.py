c5dda4412d27f357215a1b9797286e40e8ea4aa7
wweiphy <wei.w@cern.ch>
2023-10-31 20:23:31 -0500

    test

--- DNN_framework/evaluationScripts/plottingScripts.py
+++ DNN_framework/evaluationScripts/plottingScripts.py
@@ -67,23 +67,23 @@ class savenominalDiscriminators:
             
             current_binrange = [0,1]
             if node_cls == "ttHH":
-                current_binrange = [0, 0.23, 0.27, 0.3, 0.33, 0.55]
+                current_binrange = [0, 0.23, 0.27, 0.3, 0.33, 0.35, 0.55]
             if node_cls == "ttH":
                 current_binrange = [0, 0.18, 0.20, 0.22, 0.24, 0.28, 0.45]
             if node_cls == "ttZH":
                 current_binrange = [0, 0.2, 0.23, 0.25, 0.28, 0.45]
             if node_cls == "ttZ":
-                current_binrange = [0, 0.19, 0.21, 0.25, 0.28, 0.45]
+                current_binrange = [0, 0.19, 0.22, 0.25, 0.27, 0.45]
             if node_cls == "ttZZ":
-                current_binrange = [0, 0.18, 0.20, 0.23, 0.26, 0.27, 0.5]
+                current_binrange = [0, 0.18, 0.20, 0.22, 0.25, 0.27, 0.5]
             if node_cls == "ttlf":
-                current_binrange = [0, 0.22, 0.3, 0.34, 0.4, 0.7]
+                current_binrange = [0, 0.22, 0.28, 0.34, 0.4, 0.7]
             if node_cls == "ttcc":
-                current_binrange = [0, 0.17, 0.2, 0.24, 0.27, 0.32, 0.5]
+                current_binrange = [0, 0.17, 0.2, 0.24, 0.28, 0.32, 0.5]
             if node_cls == "ttmb":
-                current_binrange = [0, 0.18, 0.2, 0.23, 0.24, 0.28, 0.4]
+                current_binrange = [0, 0.18, 0.2, 0.23, 0.26, 0.28, 0.4]
             if node_cls == "ttnb":
-                current_binrange = [0, 0.23, 0.26, 0.3, 0.33, 0.55]
+                current_binrange = [0, 0.21, 0.22, 0.26, 0.34, 0.55]
             
             # print(self.data.df_unsplit_preprocessing['DNN_OutPut_{}'.format(
                 # nodeIndex)][0])
