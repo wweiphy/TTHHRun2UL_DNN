008f786e14d0746699d0ff5369ec7eccc2c05af2
wweiphy <wei.w@cern.ch>
2024-02-06 07:18:53 +0100

    update

--- run_scripts/plotting_tt4b.py
+++ run_scripts/plotting_tt4b.py
@@ -10,7 +10,7 @@
 # python plotting_tt4b.py -i Eval_0119_UL_nominal_5FS -o tt4b_2017_5FS -c ge4j_ge3t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables -n ge4j_ge3t_ttH --plot --printroc
 
 # 2018
-# python plotting_tt4b.py -i Eval_0308_UL_nominal -o tt4b_2018_normal_2 -c ge4j_ge3t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables -n ge4j_ge3t_ttH --plot --printroc --lumi=119.66 --ttnb=1.351130313 
+# python plotting_tt4b.py -i Eval_0308_UL_nominal -o tt4b_2018 -c ge4j_ge3t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables -n ge4j_ge3t_ttH --plot --printroc --lumi=119.66 --ttnb=1.351130313 
 
 # 2018 4FS tt4b
 # python plotting_tt4b.py -i Eval_0308_UL_nominal_4FS -o tt4b_2018_4FS -c ge4j_ge3t -v variables -n ge4j_ge3t_ttH --epochs=500 --signalclass=ttHH -f 0.2 -v variables -n ge4j_ge3t_ttH --plot --printroc
@@ -118,11 +118,7 @@ dnn = DNN.DNN(
     # lumi=67.24,  # 2016post
     # lumi = 78.08, # 2016pre
     # lumi = 82.96,
-<<<<<<< HEAD
-    lumi=options.lumi,
-=======
     lumi=options.getLumi(),
->>>>>>> a596b06474b6acf6d992db913f8155b2333b0470
     # lumi = 1.,
     train_epochs    = options.getTrainEpochs(),
     # metrics for evaluation (c.f. KERAS metrics)
