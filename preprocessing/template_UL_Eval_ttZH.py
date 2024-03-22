f0a210ebe95fc94f20d7e8c279abc3e450287d43
wweiphy <wei.w@cern.ch>
2024-03-22 14:23:53 -0500

    update

--- preprocessing/template_UL_Eval_ttZH.py
+++ preprocessing/template_UL_Eval_ttZH.py
@@ -128,7 +128,7 @@ dataset.addBaseSelection(base_selection)
 dataset.addSample(
     sampleName="TTZH",
     ntuples=ntuplesPath2 +
-    "/2018/ntuple/TTZHTo4b_TuneCP5_13TeV-madgraph-pythia8/sl_LEG_ntuple_2018/240322_015524/*/*nominal*.root",
+    "/2018/ntuple/TTZHTo4b_TuneCP5_13TeV-madgraph-pythia8/sl_LEG_ntuple_2018/240321_052529/*/*nominal*.root",
     #    ntuples     = ntuplesPath+"/ttH_220208.root",
     categories=ttZH_categories,
     process = "ttZH",
