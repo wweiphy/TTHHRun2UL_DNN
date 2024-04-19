4a399752c05fa5b67a6bb36ccf2b31f4d28e49f1
wweiphy <wei.w@cern.ch>
2024-04-18 22:49:51 -0500

    update

--- datacard/combineRun2/combineRun2_script.py
+++ datacard/combineRun2/combineRun2_script.py
@@ -150,12 +150,12 @@ for node in processlist:
 
             uphistoname1 = 'ljets_ge4j_ge3t_'+node+"_node__"+process+"__"+decorrelate_syst+"_2018"+"Up"
             downhistoname1 = 'ljets_ge4j_ge3t_'+node+"_node__"+process+"__"+decorrelate_syst+"_2018"+"Down"
-            print(uphistoname1)
+            # print(uphistoname1)
             testhist1 = file1.Get('ljets_ge4j_ge3t_ttHH_node__ttHH__effTrigger_mu_2018Up')
             # testhist2 = file2.Get('ljets_ge4j_ge3t_ttHH_node__ttHH__effTrigger_mu_2018Up')
-            print(testhist1)
+            # print(testhist1)
             # print(testhist2)
-
+            print('done')
             syst_histup1 = file1.Get(uphistoname1)
             syst_histdown1 = file1.Get(downhistoname1)
 
