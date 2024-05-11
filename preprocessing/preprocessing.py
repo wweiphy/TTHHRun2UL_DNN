a7fc2c00797563d126fdf68c735560a4acd77a74
wweiphy <wei.w@cern.ch>
2024-05-10 23:12:07 -0500

    update

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -895,7 +895,7 @@ class Dataset:
                     print(jet_flavor['Jet_Flav'][i][j])
                     print(jet_pt['Jet_Pt'][i][j])
 
-                    if jet_flavor['Jet_Flav'][i][j]
+                    # if jet_flavor['Jet_Flav'][i][j]
                     # btagging SF	
                     jet_btagsf_perevent *= btvjson["deepJet_shape"].evaluate("central", int(jet_flavor['Jet_Flav'][i][j]), abs(float(jet_eta['Jet_Eta'][i][j])), float(jet_pt['Jet_Pt'][i][j]), float(jet_bTag['Jet_CSV'][i][j]))
 
