e6f0346c6eb26af7c65b0340acd880b7a8938f59
wweiphy <wei.w@cern.ch>
2024-07-15 22:52:30 -0700

    Update

--- preprocessing/template_UL_Eval_singleele_2017.py
+++ preprocessing/template_UL_Eval_singleele_2017.py
@@ -67,8 +67,11 @@ base = "(N_Jets >= 4 and N_BTagsM >= 3 and Evt_MET > 20.)"
 if options.dataEra == "2017" or options.dataEra == 2017:
 
     # single lepton selections
+    # single_mu_sel = "(N_LooseElectrons == 0 and N_TightMuons == 1 and Muon_Pt > 29. and Triggered_HLT_IsoMu27_vX == 1)"
+    # single_el_sel = "(N_LooseMuons == 0 and N_TightElectrons == 1 and (Triggered_HLT_Ele35_WPTight_Gsf_vX == 1 or Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX == 1))"
+
     single_mu_sel = "(N_LooseElectrons == 0 and N_TightMuons == 1 and Muon_Pt > 29. and Triggered_HLT_IsoMu27_vX == 1)"
-    single_el_sel = "(N_LooseMuons == 0 and N_TightElectrons == 1 and (Triggered_HLT_Ele35_WPTight_Gsf_vX == 1 or Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX == 1))"
+    single_el_sel = "((N_LooseMuons == 0 and N_TightElectrons == 1 and Triggered_HLT_Ele28_eta2p1_WPTight_Gsf_HT150_vX == 1) or (N_LooseMuons == 0 and N_TightElectrons == 1 and Triggered_HLT_Ele32_WPTight_Gsf_L1DoubleEG_vX == 1 and Triggered_HLT_Ele32_WPTight_Gsf_2017SeedsX == 1))"
 
 elif options.dataEra == "2018" or options.dataEra == 2018:
     # single lepton selections
