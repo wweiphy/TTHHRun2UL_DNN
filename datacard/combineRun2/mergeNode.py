62baa761a1ea91713726b427164fe1f81a3d9074
wweiphy <wei.w@cern.ch>
2024-10-10 15:22:27 -0700

    test

--- datacard/combineRun2/mergeNode.py
+++ datacard/combineRun2/mergeNode.py
@@ -39,7 +39,7 @@ sys.path.append(basedir)
 folder_path = basedir + "/workdir/"
 
 # files = ['230220','230119','230515','230523']
-files = ['230119']
+files = ['230220']
 histofile = "histo-name-merge.csv"
 decorrelated_systs = ['effTrigger_mu','effTrigger_e','eff_mu','eff_e','btag_hfstats1','btag_hfstats2','btag_lfstats1','btag_lfstats2','JER']
 
