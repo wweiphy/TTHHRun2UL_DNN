9b7eb00f9224ff3aaf86b3c09ccd392f1c662982
wweiphy <wei.w@cern.ch>
2024-11-10 20:05:29 -0800

    Update

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1917,7 +1917,7 @@ class Dataset:
 
             electrons_Prompt_PassesID = 0
 
-            print(electrons_Prompt["ElectronDL_isPrompt"][i].size)
+            print("event ",i,", electron number is ", electrons_Prompt["ElectronDL_isPrompt"][i].size)
 
             if electrons_Prompt["ElectronDL_isPrompt"][i].size != 0: 
 
