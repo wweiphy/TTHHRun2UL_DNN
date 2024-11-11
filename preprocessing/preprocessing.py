4fb482c41a1465d6376ad1c99f09bf781e4fcde6
wweiphy <wei.w@cern.ch>
2024-11-10 20:10:01 -0800

    test

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1912,7 +1912,7 @@ class Dataset:
         electrons_PassesID = tree.pandas.df("ElectronDL_passesID")
 
         ele_Prompt_PassesID = []
-        print(electrons_Prompt.size)
+        print(electrons_Prompt["ElectronDL_isPrompt"][61].size)
 
         for i in range(electrons_Prompt.size):
 
